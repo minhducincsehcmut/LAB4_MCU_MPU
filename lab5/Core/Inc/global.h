a5992e7789ebb5e05404a1b3d7efe0e5516bba1e
Huỳnh Lâm Minh Đức <80137841+minhducincsehcmut@users.noreply.github.com>
2022-11-30 13:39:16 +0700

    LAB4_STM32_IDE

---
+++ lab5/Core/Inc/global.h
@@ -0,0 +1,30 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 30, 2022
+ *      Author: duchu
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#define SCH_MAX_TASKS 					5
+#define DESIRED_NUMBER_OF_TASKS 		2
+#define INIT_ADD 						0
+#define PERIODIC_ADD 					1
+
+typedef struct {
+	void (*pTask)(void);
+	uint32_t Delay;
+	uint32_t Period;
+	uint8_t RunMe;
+	uint8_t TaskID;
+	//uint8_t Taskpos;
+} sTask;
+
+typedef struct {
+	sTask TASK_QUEUE[SCH_MAX_TASKS];
+	int numofTask;
+} taskList;
+
+#endif /* INC_GLOBAL_H_ */
---
+++ lab5/Core/Src/task.c
@@ -0,0 +1,29 @@
+/*
+ * task.c
+ *
+ *  Created on: Nov 30, 2022
+ *      Author: duchu
+ */
+
+#include "main.h"
+#include "global.h"
+#include "task.h"
+#include <stdio.h>
+
+void task_display1() {
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+}
+void task_display2() {
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+}
+void task_display3() {
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_7);
+}
+void task_display4() {
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_8);
+}
+void task_display5() {
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_9);
+}
+
+
